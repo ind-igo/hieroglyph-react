8590af37f8289d35099bd2975875f840e4387050
Thomas Amlee <thomasamlee@gmail.com>
2019-07-06 15:54:00 -0500

    installed express-session

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const { SERVER_PORT } = process.env;
 
 const app = express();
 
+// middleware here
+app.use(
+	session({
+		secret: SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true
+	})
+);
+
 // endpoints here
+app.post('/api/register', authController.register);
+app.post('/api/login', authController.login);
+app.post('/api/signout', authController.signout);
+app.get('/api/user', authController.getUser);
 
 app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
